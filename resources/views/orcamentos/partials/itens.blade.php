ae8c79b9a4a0070f0f65a42b608378fd1fa5a322
Bezerra <bezerrabarbosarafael@gmail.com>
2025-05-08 18:01:14 -0400

    troca de título

--- resources/views/orcamentos/partials/itens.blade.php
+++ resources/views/orcamentos/partials/itens.blade.php
@@ -1,7 +1,7 @@
 <section>
     <header>
         <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-            {{ __('Itens') }}
+            {{ __('Materiais empregados em obra') }}
         </h2>
     </header>
 
